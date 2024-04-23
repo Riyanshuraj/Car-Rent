b42f03c0ba37fa8b44b34339da67f272e4a5360f
Riyanshuraj <167606196+Riyanshuraj@users.noreply.github.com>
2024-04-23 12:53:31 +0530

    CARrEANTAL

--- config/config.js
+++ config/config.js
@@ -3,7 +3,9 @@ const env = process.env.NODE_ENV || 'development'
 const config = {
     development: {
         port: process.env.PORT || 9990,
-        dbUrl: 'mongodb+srv://user2:123454321@cluster0.t7dpb.mongodb.net/Tripps_ExamPrep1?retryWrites=true&w=majority',
+        dbUrl: 'mongodb://localhost:27017/',
+        //dbUrl: 'mongodb+srv://admin:uzfHirM6SnIUA9El@cluster0.8cre9er.mongodb.net/',
+        //dbUrl: 'mongodb+srv://user2:123454321@cluster0.t7dpb.mongodb.net/Tripps_ExamPrep1?retryWrites=true&w=majority',
         cookie: 'x-auth-token',
         secret: 'NodeJS'
     },
